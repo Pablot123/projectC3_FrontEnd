82da25e97a29274697627dbddc78f3375ff469b6
Pablot123 <48389551+Pablot123@users.noreply.github.com>
2020-12-18 00:07:20 -0500

    Update reservar.vue

--- src/components/reservar.vue
+++ src/components/reservar.vue
@@ -4,44 +4,27 @@
         <form>
             <fieldset>
                 <legend>Datos del usuario</legend>
-                <div class="form-group">
-                    <label>Nombre: </label>
-                    <input type="email" class="form-control" id="nombre_usuario" aria-describedby="emailHelp" placeholder="Nombre">
-                </div>
-                <div class="form-group">
-                    <label>Apellido: </label>
-                    <input type="email" class="form-control" id="apellido_usuario" aria-describedby="emailHelp" placeholder="Apellido">
-                </div>
-                <div class="form-group">
-                    <label>Email address: </label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="email@example.com">
-                </div>
-                <div class="form-group">
-                    <label >Telefono: </label>
-                    <input type="email" class="form-control" id="numero_telefono" aria-describedby="emailHelp" placeholder="Telefono">
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password: </label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                </div>
-                <div class="form-group">
-                    <label>Tipo de documento: </label>
-                    <select class="form-control" id="tipo_documento" >
-                        <option>Seleccione...</option>
-                        <option>Cedula de ciudadania</option>
-                        <option>Pasaporte</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label >Numero de documento: </label>
-                    <input type="email" class="form-control" id="numero_identificacion" aria-describedby="emailHelp" placeholder="Telefono">
-                </div>
+                <label for="fname">Nombre</label>
+                <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+                <label for="lname">Apellido</label>
+                <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+                <label>Email address: </label>
+                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="email@example.com">
+                <label >Telefono: </label>
+                <input type="text" class="form-control" id="numero_telefono" aria-describedby="emailHelp" placeholder="Telefono">
+                <label>Tipo de documento: </label>
+                <select class="form-control" id="tipo_documento" >
+                    <option>Seleccione...</option>
+                    <option>Cedula de ciudadania</option>
+                    <option>Pasaporte</option>
+                </select>
+                <label >Numero de documento: </label>
+                <input type="text" class="form-control" id="numero_identificacion" aria-describedby="emailHelp" placeholder="Identificacion">
             </fieldset>
             <br>
             <br>
-
-
-
+            
             <fieldset class="form-group">
                 <legend>Datos de la reserva</legend>
                 <div>
@@ -127,6 +110,41 @@ li {
 }
 .button2:hover {
   box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+}
+form{
+    width: 50%;
+    margin-left: auto;
+    margin-right: auto;
+
+}
+body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box;}
+
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type=submit] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+fieldset{
+    background-color: #f2f2f2;
 }
  
 </style>
